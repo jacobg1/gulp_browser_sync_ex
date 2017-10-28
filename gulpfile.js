f02e57b15b6349e3a84105b269848799366322c3
Jacob Greenwald <jeg4t@virginia.edu>
2017-10-28 15:25:04 -0400

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+const gulp = require('gulp')
+const browserSync = require('browser-sync').create()
+const reload = browserSync.reload
+// Static server
+gulp.task('browser-sync', function () {
+  browserSync.init({
+    server: {
+      baseDir: './'
+    }
+  })
+  gulp.watch('*.html').on('change', reload)
+})
