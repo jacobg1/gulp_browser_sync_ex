0488931db3a6716cb560024aa6b3ec3a9f8f4ce2
Jacob Greenwald <jeg4t@virginia.edu>
2017-10-28 16:33:44 -0400

    watch css

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp')
 const browserSync = require('browser-sync').create()
 const reload = browserSync.reload
+
 // Static server
 gulp.task('browser-sync', function () {
   browserSync.init({
@@ -8,5 +9,16 @@ gulp.task('browser-sync', function () {
       baseDir: './'
     }
   })
+})
+
+gulp.task('watch', function () {
+  // watch html
   gulp.watch('*.html').on('change', reload)
+
+  // watch css
+  gulp.watch('*.css').on('change', reload)
+})
+
+gulp.task('default', function () {
+  gulp.start('browser-sync', 'watch')
 })
