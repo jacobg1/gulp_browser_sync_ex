693cecaa856a9f26eaf011353eba57983d73e9af
Jacob Greenwald <jeg4t@virginia.edu>
2017-10-28 16:43:29 -0400

    add sass processing

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 const gulp = require('gulp')
+const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create()
 const reload = browserSync.reload
 
@@ -12,13 +13,25 @@ gulp.task('browser-sync', function () {
 })
 
 gulp.task('watch', function () {
+
   // watch html
   gulp.watch('*.html').on('change', reload)
 
-  // watch css
+  // watch scss
+  gulp.watch('*.scss', ['sass'])
+
+  //reload after css change
   gulp.watch('*.css').on('change', reload)
+
+})
+
+//process scss
+gulp.task('sass', function () {
+  return gulp.src('*.scss')
+  .pipe(sass().on('error', sass.logError))
+  .pipe(gulp.dest('./'))
 })
 
 gulp.task('default', function () {
-  gulp.start('browser-sync', 'watch')
+  gulp.start('browser-sync', 'sass', 'watch')
 })
